d9bb224524fd6fc4d9c585c93264519b978b55cf
suat.karakusoglu <suatkarakusoglu@gmail.com>
2017-09-11 10:54:29 +0300

    Initial Commit

---
+++ SKCollectionView/SKCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  SKCollectionView.h
+//  SKCollectionView
+//
+//  Created by suat.karakusoglu on 11.09.17.
+//  Copyright © 2017 suat.karakusoglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKCollectionView.
+FOUNDATION_EXPORT double SKCollectionViewVersionNumber;
+
+//! Project version string for SKCollectionView.
+FOUNDATION_EXPORT const unsigned char SKCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKCollectionView/PublicHeader.h>
+
+
