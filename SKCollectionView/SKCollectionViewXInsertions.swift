4e37689b57277c6194d90fd9d8bfa45df52242bd
Suat Karakusoglu <suatkarakusoglu@gmail.com>
2018-03-28 13:51:33 +0300

    Before model insertion off by one error is fixed

--- SKCollectionView/SKCollectionViewXInsertions.swift
+++ SKCollectionView/SKCollectionViewXInsertions.swift
@@ -66,7 +66,7 @@ extension SKCollectionView
     public func skInsertModel(model: SKCollectionModel, beforeModel: SKCollectionModel, scrollToIt: Bool = false)
     {
         guard let indexPathOfModel = self.skGetIndexPathOfModel(collectionModelToFindIndex: beforeModel) else { return }
-        let rowToInsert = max(indexPathOfModel.row - 1, 0)
+        let rowToInsert = max(indexPathOfModel.row, 0)
         let indexPathToInsert = IndexPath(row: rowToInsert, section: indexPathOfModel.section)
         self.skInsertModel(model: model, indexPath: indexPathToInsert, scrollToIt: scrollToIt)
     }
