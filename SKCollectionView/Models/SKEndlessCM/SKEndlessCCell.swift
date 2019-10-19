3c9e78de189b9db74fd88a352ae51f2b55f9d065
Suat Karakuşoğlu <suatkarakusoglu@gmail.com>
2019-10-20 00:05:30 +0300

    Activity indicator animation is started always.

--- SKCollectionView/Models/SKEndlessCM/SKEndlessCCell.swift
+++ SKCollectionView/Models/SKEndlessCM/SKEndlessCCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class SKEndlessCCell: SKCollectionCell
 {
-    @IBOutlet weak var imageViewLoading: UIImageView!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
     
     override func awakeFromNib()
     {
@@ -21,17 +21,6 @@ class SKEndlessCCell: SKCollectionCell
     {
         guard let model = kollectionModel as? SKEndlessCModel else { return }
         super.applyModel(kollectionModel: model)
-        self.imageViewLoading.image = model.loadingImage
-        self.addLoadingScrollAnimationToView(view: self.imageViewLoading)
-    }
-    
-    private func addLoadingScrollAnimationToView(view: UIView)
-    {
-        let basicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
-        basicAnimation.toValue = NSNumber(value: .pi * 2.0 * 1.0 * 1.0)
-        basicAnimation.duration = 1
-        basicAnimation.isCumulative = true
-        basicAnimation.repeatCount = 100
-        view.layer.add(basicAnimation, forKey: "rotationAnimation")
+        self.activityIndicatorView.startAnimating()
     }
 }
