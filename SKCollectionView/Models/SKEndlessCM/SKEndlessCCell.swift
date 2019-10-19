a558f538b31013d8f81ccad7f3934858dd7b9a94
Suat Karakuşoğlu <suatkarakusoglu@gmail.com>
2019-10-19 23:36:28 +0300

    Revert "Activity indicator is more appropriate was loading view."
    This reverts commit a2e281ff6645bcfd4b421e96162b1c4fb44da1d2.

--- SKCollectionView/Models/SKEndlessCM/SKEndlessCCell.swift
+++ SKCollectionView/Models/SKEndlessCM/SKEndlessCCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SKEndlessCCell: SKCollectionCell
 {
+    @IBOutlet weak var imageViewLoading: UIImageView!
+    
     override func awakeFromNib()
     {
         super.awakeFromNib()
@@ -19,5 +21,17 @@ class SKEndlessCCell: SKCollectionCell
     {
         guard let model = kollectionModel as? SKEndlessCModel else { return }
         super.applyModel(kollectionModel: model)
+        self.imageViewLoading.image = model.loadingImage
+        self.addLoadingScrollAnimationToView(view: self.imageViewLoading)
+    }
+    
+    private func addLoadingScrollAnimationToView(view: UIView)
+    {
+        let basicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        basicAnimation.toValue = NSNumber(value: .pi * 2.0 * 1.0 * 1.0)
+        basicAnimation.duration = 1
+        basicAnimation.isCumulative = true
+        basicAnimation.repeatCount = 100
+        view.layer.add(basicAnimation, forKey: "rotationAnimation")
     }
 }
